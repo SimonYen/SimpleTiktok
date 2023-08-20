7045a178e96c6d140dc71ba8e2767b30b71d5ade
Simon Yen <simonyen@foxmail.com>
2023-08-20 16:45:41 +0800

    修复视频流评论个数为0的bug

--- construct/video.go
+++ construct/video.go
@@ -11,6 +11,9 @@ import (
 func VideoJSON(video_id, operator_id uint) models.VideoJSON {
 	//查询视频信息
 	v := new(models.Video)
+	//查询视频评论个数
+	var comments []models.Comment
+	database.Handler.Where("video_id = ?", video_id).Find(&comments)
 	database.Handler.Where("id = ?", video_id).First(v)
 	//卧槽，真的清爽多了
 	return models.VideoJSON{
@@ -19,7 +22,7 @@ func VideoJSON(video_id, operator_id uint) models.VideoJSON {
 		PlayURL:       fmt.Sprintf("%s:%d/public/video/%d%s", config.Server.Host, config.Server.Port, v.Id, v.Extension),
 		CoverURL:      fmt.Sprintf("%s:%d/public/screenshot/%d.png", config.Server.Host, config.Server.Port, v.Id),
 		FavoriteCount: utils.GetVideoLikeCount(v.Id),
-		CommentCount:  0,
+		CommentCount:  int64(len(comments)),
 		IsFavorite:    utils.VideoIsLiked(v.Id, operator_id),
 		Title:         v.Title,
 	}
